1e3eaaf2ad16006fb975b54812c7bdf9e2e89f20
jsgeekes <jsgeekes@gmail.com>
2020-09-30 22:22:48 +0300

    config project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+const path = require('path')
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [{
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          "css-loader"
+        ]
+      }
+    ],
+  },
+  externals: {
+    react: 'react'
+  }
+}
